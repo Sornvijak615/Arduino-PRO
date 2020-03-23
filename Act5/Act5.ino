8249acf2cb32f2e478f15f2541dc73a564928677
sornvijak615 <sornvijak2@gmail.com>
2020-03-23 15:51:30 +0700

    act5

--- Act5/Act5.ino
+++ Act5/Act5.ino
@@ -1,4 +1,5 @@
 void setup() {
+  int count = 0;
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, OUTPUT);
@@ -12,36 +13,35 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
+  if (digitalRead(2) == 0) {
+    count++;
+  }
+  Serial.println(count);
 }
 void loop() {
-  int count = 0;
-  for (int i = 0; i < count + 1; i++) {
-    if (digitalRead(2) == 0) {
-      count++;
-      digitalWrite(3, 1);
-      digitalWrite(4, 1);
-      digitalWrite(5, 1);
-      digitalWrite(6, 1);
-      digitalWrite(7, 1);
-      digitalWrite(8, 1);
-      digitalWrite(9, 1);
-      digitalWrite(10, 1);
-      digitalWrite(11, 1);
-      digitalWrite(12, 1);
-      digitalWrite(13, 1);
-      Serial.println(count);
-    } else {
-      digitalWrite(3, 0);
-      digitalWrite(4, 0);
-      digitalWrite(5, 0);
-      digitalWrite(6, 0);
-      digitalWrite(7, 0);
-      digitalWrite(8, 0);
-      digitalWrite(9, 0);
-      digitalWrite(10, 0);
-      digitalWrite(11, 0);
-      digitalWrite(12, 0);
-      digitalWrite(13, 0);
-    }
+  if (digitalRead(2) == 0) {
+    digitalWrite(3, 1);
+    digitalWrite(4, 1);
+    digitalWrite(5, 1);
+    digitalWrite(6, 1);
+    digitalWrite(7, 1);
+    digitalWrite(8, 1);
+    digitalWrite(9, 1);
+    digitalWrite(10, 1);
+    digitalWrite(11, 1);
+    digitalWrite(12, 1);
+    digitalWrite(13, 1);
+  } else {
+    digitalWrite(3, 0);
+    digitalWrite(4, 0);
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+    digitalWrite(8, 0);
+    digitalWrite(9, 0);
+    digitalWrite(10, 0);
+    digitalWrite(11, 0);
+    digitalWrite(12, 0);
+    digitalWrite(13, 0);
   }
 }
