773045973b27f2bc63e906cf4990d078ef9923ae
sornvijak615 <sornvijak2@gmail.com>
2020-03-23 11:52:54 +0700

    Act1-3

--- Act2/Act2.ino
+++ Act2/Act2.ino
@@ -1,7 +1,46 @@
-void setup(){
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, OUTPUT);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
+int a = 250;
+int b = 500;
 void loop(){
-    
+    digitalWrite(2, 1);
+    digitalWrite(3, 1);
+    digitalWrite(4, 1);
+    digitalWrite(5, 1);
+    digitalWrite(6, 1);
+    digitalWrite(7, 1);
+    digitalWrite(8, 0);
+    digitalWrite(9, 0);
+    digitalWrite(10, 0);
+    digitalWrite(11, 0);
+    digitalWrite(12, 0);
+    digitalWrite(13, 0);
+    delay(b);
+    digitalWrite(2, 0);
+    digitalWrite(3, 0);
+    digitalWrite(4, 0);
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+    digitalWrite(8, 1);
+    digitalWrite(9, 1);
+    digitalWrite(10, 1);
+    digitalWrite(11, 1);
+    digitalWrite(12, 1);
+    digitalWrite(13, 1);
+    delay(b);
     Serial.println("moomin");
 }
