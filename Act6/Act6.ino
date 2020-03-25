1a9c3ad074877341215cd97d978b2a5b7bc4b8cb
sornvijak615 <sornvijak2@gmail.com>
2020-03-26 02:50:22 +0700

    Act6

--- Act6/Act6.ino
+++ Act6/Act6.ino
@@ -22,7 +22,7 @@ void setup() {
 }
 int count = 0;
 void loop() {
-  if (digitalRead(2) == 1) {
+  if (digitalRead(2) == 0) {
     switch (count) {
     case 1:
       digitalWrite(8, 1);
@@ -61,7 +61,44 @@ void loop() {
       break;
     }
   } else {
-    if (digitalRead(2) == 1) {
+    switch (count) {
+    case 1:
+      digitalWrite(8, 1);
+      digitalWrite(9, 1);
+      digitalWrite(10, 1);
+      digitalWrite(11, 1);
+      digitalWrite(12, 1);
+      digitalWrite(13, 1);
+      break;
+    case 2:
+      digitalWrite(8, 1);
+      digitalWrite(9, 1);
+      digitalWrite(10, 1);
+      digitalWrite(11, 1);
+      digitalWrite(12, 1);
+      digitalWrite(13, 1);
+      delay(100);
+      digitalWrite(8, 0);
+      digitalWrite(9, 0);
+      digitalWrite(10, 0);
+      digitalWrite(11, 0);
+      digitalWrite(12, 0);
+      digitalWrite(13, 0);
+      delay(100);
+      break;
+    case 3:
+      digitalWrite(8, 0);
+      digitalWrite(9, 0);
+      digitalWrite(10, 0);
+      digitalWrite(11, 0);
+      digitalWrite(12, 0);
+      digitalWrite(13, 0);
+      break;
+    case 4:
+      count = 1;
+      break;
+    }
+    if (digitalRead(2) == 0) {
       count++;
     }
   }
