5fea53f7ababe5a0b2232c55b18e96f34f39f58a
sornvijak615 <sornvijak2@gmail.com>
2020-03-26 19:09:16 +0700

    Act8,9

--- Act9/Act9.ino
+++ Act9/Act9.ino
@@ -1,9 +1,11 @@
 void setup() {
   Serial.begin(9600);
+  int x = 0;
   int Id = 6 + 1 + 0 + 8 + 0 + 5 + 0 + 1 + 6 + 1 + 5;
-  for (int i = 1; i <= 1000; i++) {
-    if (i % Id == 0) {
-      Serial.println(i);
+  while (x <= 1000) {
+    x++;
+    if (x % Id == 0) {
+      Serial.println(x);
     }
   }
 }
