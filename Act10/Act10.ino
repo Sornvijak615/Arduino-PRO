8c2cd6a5f98075c510f89da0d88b1fb23d2ece88
sornvijak615 <sornvijak2@gmail.com>
2020-03-26 23:02:26 +0700

    Act10

--- Act10/Act10.ino
+++ Act10/Act10.ino
@@ -1,66 +1,38 @@
 void setup() {
+  Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
-bool click = true;
-int a = 0;
-int b = 255;
 void loop() {
-  if (digitalRead(2) == 0) {
-    click = !click;
-    if (click == true) {
-      for (int i = 0; i <= 255; i++) {
-        a++;
-        analogWrite(3, a);
-        analogWrite(5, a);
-        analogWrite(6, a);
-        analogWrite(9, a);
-        analogWrite(10, a);
-        analogWrite(11, a);
-        delay(7);
+  int x = 0;
+  for (x = 0; x <= 1023; x++) {
+    analogWrite(3, x);
+    analogWrite(5, x);
+    analogWrite(6, x);
+    analogWrite(9, x);
+    analogWrite(10, x);
+    analogWrite(11, x);
+    delay(20);
+    if (x == 1023) {
+      for (x = 1023; x >= 0; x--) {
+        analogWrite(3, x);
+        analogWrite(5, x);
+        analogWrite(6, x);
+        analogWrite(9, x);
+        analogWrite(10, x);
+        analogWrite(11, x);
+        delay(20);
       }
-      a = 0;
-      for (int j = 0; j <= 255; j++) {
-        b--;
-        analogWrite(3, b);
-        analogWrite(5, b);
-        analogWrite(6, b);
-        analogWrite(9, b);
-        analogWrite(10, b);
-        analogWrite(11, b);
-        delay(7);
-      }
-      b = 255;
-    } else if (click == false) {
-      analogWrite(3, 0);
-      analogWrite(5, 0);
-      analogWrite(6, 0);
-      analogWrite(9, 0);
-      analogWrite(10, 0);
-      analogWrite(11, 0);
     }
   }
-  /*for (int a = 0; a <= 255; a++) {
-    analogWrite(3, a);
-    analogWrite(5, a);
-    analogWrite(6, a);
-    analogWrite(9, a);
-    analogWrite(10, a);
-    analogWrite(11, a);
-    delay(7);
-  }
-  for (int a = 255; a >= 0; a--) {
-    analogWrite(3, a);
-    analogWrite(5, a);
-    analogWrite(6, a);
-    analogWrite(9, a);
-    analogWrite(10, a);
-    analogWrite(11, a);
-    delay(7);
-  }*/
 }
